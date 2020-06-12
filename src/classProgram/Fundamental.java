eef9d42765b0596ef6c4dc8288574e7fc4212381
saptadweepa <saptadweepa.30@gmail.com>
2020-06-12 19:59:43 +0530

    completed HW

--- src/classProgram/Fundamental.java
+++ src/classProgram/Fundamental.java
@@ -13,6 +13,12 @@ public class Fundamental {
 	 */
 	
 	public static void main(String args[]) {
+		Account acc1= new Account(123456L, "RAM", "30/1/1990", 50000 , "saving");
+		Account acc2= new Account(123456L, "SHAM", "31/2/1990", 20000 , "current");
+		Account acc3= new Account(123456l, "JODU", "1/3/1990", 80000 , "fixed");
+		acc1.printDetails();
+		acc2.printDetails();
+		acc3.printDetails();
 		   
 	}
 
@@ -26,10 +32,27 @@ class Account{                 // Class is a bluePrint of object . Objects are u
 	int avgBalance;
 	String accountType;
 	
+	Account(long accountNum, String name, String DOB, int balance, String accountType){
+		this.accountNum= accountNum;
+		this.name= name;
+		this.DOB= DOB;
+		this.balance= balance;
+		this.accountType= accountType;
+	}
+	
+	public void printDetails() {
+		System.out.println(" Account Details are : ");
+		System.out.println("Account Number: "+accountNum);
+		System.out.println("Account Name: "+name);
+		System.out.println("DOB: "+DOB);
+		System.out.println("Account Balance: "+balance);
+		System.out.println("Account Type: "+accountType);
+		System.out.println();
+	}
 	
 	
-	Account acc1= new Account();
 	
-	;
+	
+	
 	
 }
